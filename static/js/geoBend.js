2acb7f140fe2da1d9dd231b53d15fa182e17c111
holistudio <daniel.bin.lu@gmail.com>
2023-12-19 22:55:30 -0500

    Comments to main()

--- static/js/geoBend.js
+++ static/js/geoBend.js
@@ -26,39 +26,42 @@ let poses = [];
 
 
 function main() {
-    //RENDERER
+
+    // THREEJS RENDERER
     const canvas = document.querySelector('#c'); //make sure canvas is defined before this script
     const renderer = new THREE.WebGLRenderer({canvas}); //sets renderer's DOM element
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
     renderer.setClearColor(0xFFFFFF);
 
-    //CAMERA
+    // THREEJS CAMERA
     const fov = 40;
     const aspect = 2;  // the canvas default
     const near = 1;
     const far = 10000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-
+    
+    const lookAt = [1053,800,-300]; // the XYZ coordinates of the point the camera is looking at (NOT the camera's direction vector)
     // const lookAt = [1053,-2,-660]
-    const lookAt = [1053,800,-300]
-    const lookUnitVector = [0.67236, 0.694525, 0.25606]
-    const zoom = 0.5
-    const distance = 500 / zoom
+
+    const camDirVector = [0.67236, 0.694525, 0.25606]; // the direction the camera is pointing towards (camera's direction vector)
+    const zoom = 0.5; // zoom factor, basically moves the camera closer if zoom value is larger
+    const distance = 500 / zoom; // set the distance relative to the "default" distance of 500 units, based on the zoom value
     
-    // camera.position.set(3965, 3006, 449); 
-    camera.position.set(lookAt[0]+ distance * lookUnitVector[0], 
-        lookAt[1] + distance * lookUnitVector[1], 
-        lookAt[2] + distance * lookUnitVector[2]); 
-    // camera.position.set(3808, 3007, 548);
-    // camera.lookAt(zoom * lookUnitVector[0], zoom * lookUnitVector[1], zoom * lookUnitVector[2]);
+    // set the camera's position based on the lookAt point, camera direction, and distance away from the lookAt point
+    camera.position.set(lookAt[0]+ distance * camDirVector[0], 
+        lookAt[1] + distance * camDirVector[1], 
+        lookAt[2] + distance * camDirVector[2]); 
+
+    // set the camera's lookAt point
     camera.lookAt(lookAt[0],lookAt[1],lookAt[2]);
-    
-    //SCENE
+
+    // THREEJS SCENE
     const scene = new THREE.Scene();
 
-    //LIGHTS
+    // THREEJS LIGHTS
     {
+        // white directional light
         const color = 0xFFFFFF;
         const intensity = 1;
         const light = new THREE.DirectionalLight(color, intensity);
@@ -66,8 +69,9 @@ function main() {
         light.position.set(-1000,1000,1000);
         scene.add(light);
 
-        const helper = new THREE.DirectionalLightHelper( light, 5 );
+        // const helper = new THREE.DirectionalLightHelper( light, 5 );
 
+        // blue point light for two-color gradient effect
         const pointLight = new THREE.PointLight(0x0000FF, 0.5, 0 );
         // pointLight.position.set( 1500, -1000, -3000 );
         pointLight.position.set(1053,1000,0);
@@ -77,8 +81,12 @@ function main() {
         // scene.add(pointLightHelper);
     }
 
-    //SURFACES
+    // THREEJS SURFACES
+
+    // blank geometry buffer
     geometry = new THREE.BufferGeometry();
+
+    // mesh material
     const meshMaterial = new THREE.MeshLambertMaterial( {color: 0x32fcdb, side: THREE.DoubleSide} );
 
     //canvas' "external resolution" / size on the webpage is set by CSS
@@ -95,6 +103,8 @@ function main() {
         return needResize; //used for updating other thing if canvas is resized
     }
 
+    // This function makes the quad mesh the first time around 
+    // before the preset number of poses are loaded/detected (numCurves)
     function makeQuad(p1,p2,p3,p4) {
         // create a simple square shape. We duplicate the top left and bottom right
         // vertices because each vertex needs to appear once per triangle.
@@ -117,6 +127,7 @@ function main() {
     
         const crossAB = {x:a2 * b3 - a3 * b2, y:a3 * b1 - a1 * b3, z:a1 * b2 - a2 * b1};
         
+        // push normal vectors to list of normal vectors
         normals.push(crossAB.x, crossAB.y, crossAB.z);
         normals.push(crossAB.x, crossAB.y, crossAB.z);
         normals.push(crossAB.x, crossAB.y, crossAB.z);
@@ -125,6 +136,8 @@ function main() {
         normals.push(crossAB.x, crossAB.y, crossAB.z);
     }
     
+    // This function modifies the existing quad mesh already displayed
+    // after the preset number of poses are loaded/detected (numCurves)
     function updateQuad(i,p1,p2,p3,p4) {
         const position = geometry.attributes.position;
         const dir = geometry.attributes.normal;
@@ -192,6 +205,8 @@ function main() {
         dir.setZ(i+5, crossAB.z);
     }
 
+    // This function loads a pose detected by PoseNet
+    // as curve points
     function loadPose(){
         let curve = {id:curveKey, points:[]};
 
@@ -234,11 +249,13 @@ function main() {
         return curve;
     }
 
+    // THREEJS Render Loop
     function render(time){
-        // Calculate number of seconds that have elapsed
+        // Calculate time that has elapsed in units of seconds
         let seconds = (time-startTime)/1000
 
-        // Print seconds every 1 second
+        // At the update rate (Hz), load a new pose as a new curve
+        // and pop the oldest pose/curve off/out of the the mesh
         if (Math.floor(seconds) > secondCounter){
             console.log(seconds);
             secondCounter+= 0.5;
@@ -270,9 +287,16 @@ function main() {
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
 
+        // bool formWindowComplete is true if enough poses have been detected to form a mesh with enough curves (numCurves)
+
+        // if not enough poses have been detected
         if(!formWindowComplete){
+            // check if enough poses have been detected
             if (formWindowPoints.length == numCurves){
+
+                // Make quad mesh between each pose/curve in the formWindowPoints (i.e. linear lofting of curves)
                 for (let i = 0; i < formWindowPoints.length-1; i++) {
+
                     const curve0 = formWindowPoints[i];
                     const curve1 = formWindowPoints[i+1];
                     
@@ -293,18 +317,28 @@ function main() {
                         makeQuad(p1,p2,p3,p4);
                     }
                 }
+
                 // itemSize = 3 because there are 3 values (components) per vertex
                 geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
                 geometry.setAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
                 
+                // create the mesh with that geometry
                 mesh = new THREE.Mesh( geometry, meshMaterial );
+
+                // add mesh to scene
                 scene.add(mesh);
-    
+
+                // set formWindowComplete to true so that none of this code runs ever again
                 formWindowComplete = true;
             }
+            // otherwise do nothing
         }
         else{
-            let geoIndex=0;
+            // if enough poses have been detected, 
+            // then the mesh is updated, not created/added to the scene
+
+            let geoIndex=0; // index for tracking which quad on the mesh needs to be updated
+
             for (let i = 0; i < formWindowPoints.length-1; i++) {
                 const curve0 = formWindowPoints[i];
                 const curve1 = formWindowPoints[i+1];
@@ -327,23 +361,29 @@ function main() {
                         p4 = curve0.points[j+1];
                     }
 
-                    p1 = {x: curve0X, y: p1.y, z: p1.z};
+                    // update y- and z-coordinates based on the pose, 
+                    // keep x-coordinate unchanged
+                    p1 = {x: curve0X, y: p1.y, z: p1.z}; 
                     p2 = {x: curve1X, y: p2.y, z: p2.z};
                     p3 = {x: curve1X, y: p3.y, z: p3.z};
                     p4 = {x: curve0X, y: p4.y, z: p4.z};
 
                     updateQuad(geoIndex,p1,p2,p3,p4);
-                    geoIndex = geoIndex + 6;
+
+                    geoIndex = geoIndex + 6; // increment quad index by 6
                 }
             }
+
+            // tell THREEJS the geometry needs to be updated
             geometry.attributes.position.needsUpdate = true;
             geometry.attributes.normal.needsUpdate = true;
         }
+
+        // render scene
         renderer.render(scene, camera);
-        
         requestAnimationFrame(render);
     }
-    requestAnimationFrame(render);
+    requestAnimationFrame(render); //TODO: Undedrstand why this requestAnimationFrame(render) shows up twice
 
     
 
@@ -367,8 +407,8 @@ function setup(){
     });
 
     // Hide the video element, and just show the canvas
-    video.hide();
+    // video.hide();
 
-    // Run the main function (THREEjs code using the poseNet detection data)
+    // Run the main function (THREEjs code using the PoseNet detection data)
     main();
 }
